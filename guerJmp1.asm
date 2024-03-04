23c6c7bd5f323142e5fcfde23ccdb1d510e1f575
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-03-05 00:18:44 +0800

    Added mth power of each digit

--- guerJmp1.asm
+++ guerJmp1.asm
@@ -7,6 +7,8 @@ section .data
 inputNum dq 0
 continuePrompt db 0
 digitCount db 0
+rotatedNum dq 0
+sumMthPowerDigit dq 0
 
 section .text
 global main
@@ -23,10 +25,19 @@ main:
         ; Input Validation
         CALL INPUT_VALIDATION
         
-        ; m-th power of each digits
+        ; A.) m-th power of each digits
         
-        ; 1. Count the number of digits
-        CALL COUNT_DIGIT
+            ; 1. Count the number of digits
+            CALL COUNT_DIGIT
+        
+            ; 2. Rotate input to perform order m-th power
+            CALL ROTATE_INPUT
+            
+            ; 3. Get m-th power of each digit
+            PRINT_STRING "m-th power of each digits: "
+            CALL MTH_POWER_DIGIT
+            NEWLINE
+            
         
         ; Sum of the m-th power digits
         
@@ -61,13 +72,93 @@ main:
             JE END_COUNT_DIGIT
             
             MOV RCX, 10 ; divisor
-            MOV RDX, 0
+            MOV RDX, 0 ; remainder
             DIV RCX
             INC byte [digitCount]
             
             JMP START_COUNT_DIGIT
         
         END_COUNT_DIGIT:
+            ret
+            
+    ROTATE_INPUT:
+        MOV RAX, [inputNum] ; dividend
+        MOV RBX, [digitCount]
+        START_ROTATE_INPUT:
+            CMP RBX, 0 ; Check if digitCount is 0
+            JE END_ROTATE_INPUT
+            
+            MOV RCX, 10 ; divisor
+            MOV RDX, 0 ; remainder
+            DIV RCX
+            
+            ; Get power of 10
+            MOV R8, 1
+            MOV R9, RBX ; from digitCount
+            DEC R9
+            GET_POWER_10:
+                CMP R9, 0
+                JE END_GET_POWER_10
+                
+                IMUL R8, 10
+                DEC R9
+                
+                JMP GET_POWER_10
+                    
+            END_GET_POWER_10:
+                IMUL R8, RDX
+                ADD [rotatedNum], R8
+            
+            DEC RBX
+            
+            JMP START_ROTATE_INPUT
+            
+        END_ROTATE_INPUT:
+            ret
+            
+    MTH_POWER_DIGIT:
+        MOV RAX, [rotatedNum] ; dividend
+        MOV BL, [digitCount] ; iterator for each digit
+        
+        START_MTH_POWER:
+            CMP RBX, 0
+            JE END_MTH_POWER
+            
+            MOV RCX, 10 ; divisor
+            MOV RDX, 0 ; remainder
+            DIV RCX
+            
+            ; Raise digit to mth power
+            MOV R8B, [digitCount] ; m-th power
+            DEC R8B
+            MOV R9, RDX ; Set original digit here
+            EXPONENTIATE:
+                CMP R8B, 0
+                JE END_EXPONENTIATE
+                
+                IMUL RDX, R9
+                DEC R8B
+                
+                JMP EXPONENTIATE
+            
+                END_EXPONENTIATE:
+                    PRINT_DEC 8, RDX
+                    DEC RBX
+                    CMP RBX, 0
+                    JNE COMMA_FORMAT
+                    
+                    JMP START_MTH_POWER
+                    
+                    COMMA_FORMAT:
+                        PRINT_STRING ", "
+                        JMP START_MTH_POWER
+            
+            JMP START_MTH_POWER
+        
+            END_MTH_POWER:
+                NOP
+    
+        END_MTH_POWER_DIGIT:
             ret
        
     xor rax, rax
